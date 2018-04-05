dfcb45992be3be0bcd743cf0baa30edced87f1e1
Adrian Stabiszewski <as@grundid.de>
2018-04-05 15:44:28 +0200

    image loader rewrite

--- lib/weather.dart
+++ lib/weather.dart
@@ -1,7 +1,72 @@
+import 'dart:async';
+import 'dart:collection';
 import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/src/services/asset_bundle.dart';
+
+class RadarAssetBundle extends AssetBundle {
+  final Map<String, ByteData> cache = new LinkedHashMap();
+
+  fetchRadarImage(String url) async {
+    var httpClient = new HttpClient();
+    var radarUri = Uri.parse(url);
+    var request = await httpClient.getUrl(radarUri);
+    var response = await request.close();
+    var responseLists = await response.toList();
+    var bytesList = <int>[];
+    for (var elements in responseLists) {
+      bytesList.addAll(elements);
+    }
+    return new Uint8List.fromList(bytesList);
+  }
+
+  initAssets(List<String> urls, int listenerThreshold, listener()) async {
+    var counter = 0;
+    for (var url in urls) {
+      final urlAndKey = "https://grundid.de/data/weather/" + url;
+      final imageBytes =
+          await fetchRadarImage(urlAndKey);
+      print("$urlAndKey =>  ${imageBytes.length}");
+      cache.putIfAbsent(urlAndKey, () => new ByteData.view(imageBytes.buffer));
+
+      if (counter == listenerThreshold) {
+        listener();
+      }
+      counter++;
+    }
+  }
+
+  @override
+  Future<ByteData> load(String key) {
+    final containsKey = cache.containsKey(key);
+    print("Loading key $key, contains: $containsKey");
+    return new Future.sync(() => cache[key]);
+  }
+
+  @override
+  Future<String> loadString(String key, {bool cache: true}) {
+    print("Loading string for  key $key");
+  }
+
+  @override
+  Future<T> loadStructuredData<T>(
+      String key, Future<T> Function(String value) parser) {
+    print("loadStructuredData for  key $key");
+    if (key == "AssetManifest.json") {
+      final Map<String, List<String>> jsonObject = <String, List<String>>{};
+      for (var url in cache.keys) {
+        final List<String> variants = <String>[];
+        variants.add(url);
+        jsonObject[url] = variants;
+      }
+      return parser(json.encode(jsonObject));
+    }
+  }
+}
 
 class RainViewWidget extends StatefulWidget {
   @override
@@ -10,7 +75,9 @@ class RainViewWidget extends StatefulWidget {
   }
 }
 
-class RainState extends State<RainViewWidget> with SingleTickerProviderStateMixin {
+class RainState extends State<RainViewWidget>
+    with SingleTickerProviderStateMixin {
+  RadarAssetBundle assetBundle;
 
   Animation<int> rainRadarIndex;
   AnimationController controller;
@@ -22,41 +89,43 @@ class RainState extends State<RainViewWidget> with SingleTickerProviderStateMixi
   @override
   void initState() {
     super.initState();
-    getRainUrls();
+
+    assetBundle = new RadarAssetBundle();
+    getRainUrls().then((rainUrls) {
+      radarUrls = rainUrls;
+      final duration = 2000 * radarUrls.length;
+      controller = new AnimationController(
+          duration: new Duration(milliseconds: duration), vsync: this);
+      rainRadarIndex =
+      new StepTween(begin: 0, end: radarUrls.length - 1).animate(controller)
+        ..addListener(() {
+          if (lastIndex != rainRadarIndex.value) {
+            print("listener: $rainRadarIndex.value");
+            setState(() {});
+            lastIndex = rainRadarIndex.value;
+          }
+        })
+        ..addStatusListener((status) {
+          print(status);
+        });
+
+
+      assetBundle.initAssets(rainUrls, 3, () {
+        controller.forward();
+      });
+
+    });
   }
 
   getRainUrls() async {
     var httpClient = new HttpClient();
-    var uri = new Uri.https(
-        'grundid.de', '/data/weather/files4.json',
+    var uri = new Uri.https('grundid.de', '/data/weather/files4.json',
         {'time': new DateTime.now().millisecondsSinceEpoch.toString()});
     var request = await httpClient.getUrl(uri);
     var response = await request.close();
     var responseBody = await response.transform(utf8.decoder).join();
     List<String> data = json.decode(responseBody);
-
-    data.forEach((url) {
-
-    });
-
-
-
-    radarUrls = data;
-    final duration = 2000*radarUrls.length;
-    controller = new AnimationController(
-        duration: new Duration(milliseconds: duration), vsync: this);
-    rainRadarIndex = new StepTween(begin: 0, end: radarUrls.length-1).animate(controller)
-      ..addListener(() {
-        if (lastIndex != rainRadarIndex.value) {
-          print("listener: $rainRadarIndex.value");
-          setState(() {});
-          lastIndex = rainRadarIndex.value;
-        }
-      })
-    ..addStatusListener((status) {
-      print(status);
-    });
-    controller.forward();
+    return data;
   }
 
   dispose() {
@@ -71,9 +140,13 @@ class RainState extends State<RainViewWidget> with SingleTickerProviderStateMixi
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
       ),
-      body: rainRadarIndex == null  ?  new Image.asset("images/loading.png") :
-      new Image.network("https://grundid.de/data/weather/"+radarUrls[rainRadarIndex.value], gaplessPlayback: true,) ,
+      body: rainRadarIndex == null
+          ? new Image.asset("images/loading.png")
+          : new Image.asset(
+              "https://grundid.de/data/weather/"+radarUrls[rainRadarIndex.value],
+              bundle: assetBundle,
+              gaplessPlayback: true,
+            ),
     );
   }
-
 }
